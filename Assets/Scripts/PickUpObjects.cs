58aa05334431f12916b4ee23af1b9433515e28a5
isnaya-andrade <isnayaandrade@gmail.com>
2024-11-10 00:46:33 -0600

    Funcionalidad

---
+++ Assets/Scripts/PickUpObjects.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObjects : MonoBehaviour
+{
+    public GameObject ObjectToPickUp;
+    public GameObject PickedObject;
+    public Transform interactionZone;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ObjectToPickUp != null && ObjectToPickUp.GetComponent<PickableObject>().isPickable == true && PickedObject == null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().isPickable = false;
+                PickedObject.transform.SetParent(interactionZone);
+                PickedObject.transform.position =interactionZone.position;
+                PickedObject.GetComponent <Rigidbody>().useGravity = false;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        
+        else if (PickedObject != null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().isPickable = true;
+                PickedObject.transform.SetParent(null);
+                PickedObject.GetComponent<Rigidbody>().useGravity = true;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                PickedObject = null;
+
+            }
+        }
+    }
+}
